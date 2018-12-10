e5e920e966b6a1d7ab4b946149a7e00d1820a09c
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-12-10 16:22:43 -0400

    text-classification preprocess done

---
+++ text-classification/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def load_data():
+    colnames = ["tweets", "label"]
+    data = pd.read_csv("cleantextlabels7.csv", names=colnames)
+
+    labels = data.label.tolist()
+    tweets = data.tweets.tolist()
+
+    train_tweets = tweets[0: 11001]
+    train_labels = labels[0:11001]
+
+    test_tweets = tweets[11001:]
+    test_label = labels[11001:]
+
+    return [(train_tweets, train_labels), (test_tweets, test_label)]
+
+
+
