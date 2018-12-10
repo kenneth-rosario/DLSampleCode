ba14d7f34ea585e4d48bd1c3bf4c9aa94c9b201a
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-12-10 19:45:27 -0400

    test

---
+++ text-classification/Model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense
+from preprocess import  load_data
+
+
+train_tweets = []
+train_labels = []
+test_tweets = []
+test_labels = []
+
+
+[(train_tweets, train_labels), (test_tweets, test_labels)] = load_data()
+print("Training entries: {}, labels: {}".format(len(train_tweets), len(train_labels)))
+
+# model = Sequential()
+#
+# model.add(Dense(5, input_shape=(11001, )))
+# model.add(Dense(3, activation='softmax'))
+#
+#
+# model.compile(optimizer=tf.train.AdamOptimizer(),
+#                loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+# model.fit(train_tweets,  train_labels, batch_size=64,  epochs=5)
--- text-classification/preprocess.py
+++ text-classification/preprocess.py
@@ -1,4 +1,22 @@
 import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+import numpy as np
+
+
+def convert(arg, dtype):
+    arg = tf.convert_to_tensor(
+        value=arg,
+        dtype=dtype
+    )
+    return arg
+
+def pad_str_280(arg):
+    difference = 280 - len(arg)
+    return "{:<"+str(difference)+"}".format(arg)
+
 
 def load_data():
     colnames = ["tweets", "label"]
@@ -13,15 +31,21 @@ def load_data():
     test_tweets = tweets[11001:]
     test_label = labels[11001:]
 
-    print(train_labels)
-    print("\n \n \n---------------------------------------------------------------")
+    train_tweets = [text_to_word_sequence(x) for x in train_tweets]
+    test_tweets = [text_to_word_sequence(x) for x in test_tweets]
+    t = Tokenizer(tweets)
+    t.fit_on_texts(tweets)
+    print(t.word_counts)
+    print(t.document_count)
+    print(t.word_index)
+    print(t.word_docs)
+    return [(convert(train_tweets, object), convert(train_labels, tf.int64))
+        ,(convert(test_tweets, object), convert(test_label, tf.int64))]
+
+if __name__ == '__main__':
+    load_data()
+    print("---------------------------------------------------------------")
     print("---------------------------------------------------------------")
     print("---------------------------------------------------------------")
     print("---------------------------------------------------------------")
     print("Succesfully Loaded Data into training dataset and testing")
-    return [(train_tweets, train_labels), (test_tweets, test_label)]
-
-
-
-if __name__ == '__main__':
-    load_data()
