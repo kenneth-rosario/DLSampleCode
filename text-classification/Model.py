3e10a284667127241b57d294e6aac5e0e1447ff9
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-12-10 22:29:14 -0400

    Finished model

--- text-classification/Model.py
+++ text-classification/Model.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense
-from preprocess import  load_data
-
+from tensorflow.keras.layers import  Dense, GlobalAveragePooling1D, MaxPool1D, Embedding
+from preprocess import load_data
+from tensorflow.python.keras.layers import MaxPool2D
 
 train_tweets = []
 train_labels = []
@@ -13,15 +13,19 @@ test_labels = []
 
 [(train_tweets, train_labels), (test_tweets, test_labels)] = load_data()
 
+print(train_tweets.shape)
+
 
 model = Sequential()
 
-model.add(Dense(5, input_shape=(11001, )))
-model.add(Dense(3, activation='softmax'))
+model.add(Embedding(100000, 16))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(512, activation=tf.nn.relu))
 
-
+model.add(Dense(16, activation=tf.nn.relu))
+model.add(Dense(3, activation=tf.nn.softmax))
 model.compile(optimizer=tf.train.AdamOptimizer(),
                loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_tweets,  train_labels, batch_size=64,  epochs=5)
+model.fit(train_tweets,  train_labels, batch_size=512,  epochs=40)
--- text-classification/preprocess.py
+++ text-classification/preprocess.py
@@ -1,18 +1,11 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-
+from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.text import text_to_word_sequence
+from tensorflow.keras.preprocessing.text import text_to_word_sequence, one_hot
+
 import numpy as np
-
-
-def convert(arg, dtype):
-    arg = tf.convert_to_tensor(
-        value=arg,
-        dtype=dtype
-    )
-    return arg
 
 
 def load_data():
@@ -23,21 +16,30 @@ def load_data():
     tweets = data.tweets.tolist()
 
     train_tweets = tweets[0: 11001]
-    train_labels = labels[0:11001]
+    train_labels = labels[0: 11001]
 
     test_tweets = tweets[11001:]
     test_label = labels[11001:]
 
-    t = Tokenizer()
+
+    t = Tokenizer(num_words=100000)
     t.fit_on_texts(train_tweets)
-    train_tweets = t.texts_to_matrix(tweets, mode='count')
+    train_tweets = t.texts_to_sequences(train_tweets)
     t.fit_on_texts(test_tweets)
-    test_tweets = t.texts_to_matrix(tweets, mode='count')
+    test_tweets = t.texts_to_sequences(test_tweets)
+
+
+    # train_tweets = t.texts_to_matrix(tweets, mode='')
+    # t.fit_on_texts(test_tweets)
+    # test_tweets = t.texts_to_matrix(tweets, mode='count')
+
+
 
     train_tweets =  keras.preprocessing.sequence.pad_sequences(train_tweets, value= 0, padding='post', maxlen=280)
     test_tweets = keras.preprocessing.sequence.pad_sequences(test_tweets, value=0, padding='post', maxlen=280)
+    print(train_tweets)
 
-    return [(train_tweets, train_labels),(test_tweets, test_label)]
+    return [(train_tweets, train_labels), (test_tweets, test_label)]
 
 if __name__ == '__main__':
     load_data()
